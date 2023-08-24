da91ddafe404301b1749d8fa582a19316dcc002c
hypnotoad4 <zdima761@gmail.com>
2023-08-24 18:53:33 +0300

    123

--- advertisements/app_advertisements/admin.py
+++ advertisements/app_advertisements/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Advertisement
+from datetime import datetime
+from django.utils.html import format_html
 
 class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ['id','title','description', 'price','created_at','updated_at','auction','image']
+    list_display = ['id','title','description', 'price','created_at','updated_at', 'get_updated_at_display','auction','image']
     list_filter = ['auction','created_at']
     actions = ['make_auction_false','make_auction_true']
     fieldsets = [
@@ -16,6 +18,16 @@ class AdvertisementAdmin(admin.ModelAdmin):
     @admin.action(description="Поставить возможность торга")
     def make_auction_true(self, request, queryset):
         queryset.update(auction=True)
+
+    def get_updated_at_display(self, obj):
+        updated_at = obj.updated_at
+        today = datetime.now().date()
+        if updated_at.date() == today:
+            return format_html('<span style="color:purple;">Сегодня в {}</span>', updated_at.time())
+        else:
+            return str(updated_at)
+    get_updated_at_display.short_description = 'Последнее обновление'
+    get_updated_at_display.allow_tags = True
 
 admin.site.register(Advertisement, AdvertisementAdmin)
 # Register your models here.
